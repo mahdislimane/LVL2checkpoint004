f5faea2fbf34b879ce807f6e0fb7f474d00b9f33
mahdislimane <slimane_mahdi@yahoo.fr>
2020-04-08 03:32:00 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import ProductTable from "./component/ProductTable";
+const data = [
+  {
+    category: "Electronics",
+    price: "300",
+    name: "phone"
+  },
+  {
+    category: "Electronics",
+    price: "200",
+    name: "tab"
+  },
+  {
+    category: "Clothes",
+    price: "150",
+    name: "veste"
+  },
+  {
+    category: "Clothes",
+    price: "50",
+    name: "pull"
+  }
+];
+ReactDOM.render(
+  <React.StrictMode>
+    <ProductTable product={data} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
